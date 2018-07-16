a3891cc0085263e37344931896fea13e131b9608
Christian Thomas <Christian.thomas.2894@gmail.com>
2018-07-16 13:21:01 -0600

    finishing up scrape

--- lib/top_stock_movers/stocks.rb
+++ lib/top_stock_movers/stocks.rb
@@ -1,13 +1,23 @@
 class TopStockMovers::Stocks
-  attr_accessor :ticker_symbol, :name, :price, :percent_change, :change, :rating, :sector, :description
+  attr_accessor :ticker_symbol, :name, :price, :percent_change, :change, :rating, :sector, :url
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+  
 
   def self.scrape_tradingview
     doc = Nokogiri::HTML(open("https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/"))
-    stocks = []
 
     doc.css('body div div#js-category-content div div div div#js-screener-container div table tbody tr').each do |row|
-      stock_info = row.css('td').collect{|td| td.text}
+
       stock = self.new
+      stock.url = "https://www.tradingview.com#{row.css('a').attr('href').value}"
+
+      stock_info = row.css('td').collect{|td| td.text}
+
       stock.ticker_symbol = stock_info[0].split("\n\t\t\t\t\t\t").reject{|c| c.empty?}[0]
       stock.name = stock_info[0].split("\n\t\t\t\t\t\t").reject{|c| c.empty?}[1].split(" -")[0]
       stock.price = "$#{stock_info[1]}"
@@ -15,7 +25,7 @@ class TopStockMovers::Stocks
       stock.change = stock_info[3]
       stock.rating = stock_info[4]
       stock.sector = stock_info[10]
-      binding.pry
+      @@all << self
     end
 
 
